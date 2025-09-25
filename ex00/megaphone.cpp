11a0fbc19c561cfb51e623104b1ef5ab16ce82ef
oussama7007 <oussamatachakourt@gmail.com>
2025-09-25 23:57:33 +0100

    i think i fix the first ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 23:20:35 by oait-si-          #+#    #+#             */
+/*   Updated: 2025/09/25 23:56:52 by oait-si-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char *av[])
+{
+       if(ac >= 2)
+        {
+            for(int j = 1; j < ac; j++)
+            {
+                for(int i = 0; av[j][i]; i++)
+                {
+                    if(av[j][i] >= 'a' && av[j][i] <= 'z')
+                        av[j][i] -= 32 ;
+                    std::cout << av[j][i];
+                } 
+            }
+        }
+        else 
+            std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        std::cout << '\n';
+}
