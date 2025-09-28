f94831321ae6ba9a220baaf7257f3be7d9c09f6c
oussama7007 <oussamatachakourt@gmail.com>
2025-09-28 23:59:52 +0100

    D

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,17 +6,11 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 00:39:54 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/09/26 18:41:23 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/09/28 23:59:35 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-// int main()
-// {
-//     std::string contacts[8];
-    
-// }
 
 
 
@@ -25,21 +19,22 @@
 class FirstClass
 {
     public:
-        FirstClass();
-        ~FirstClass();
+        FirstClass(); wwqwrqwere
+        ~FirstClass();b wrqwerqwer
         void    print(std::string txt);
 };
 
 class SecondClass
 {
     public:
-        SecondClass();
+        Class();
         ~SecondClass();
         void    print(void);
 };
 
 int main(void)
-{
+{  
+    
     SecondClass obj1;
     FirstClass obj2;
 
