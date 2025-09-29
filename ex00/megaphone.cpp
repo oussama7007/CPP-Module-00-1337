32935b520a1c0fcc48cce6768ccb7d3ec4746108
oussama7007 <oussamatachakourt@gmail.com>
2025-09-29 11:30:55 +0100

    still

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 23:20:35 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/09/26 15:47:31 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/09/29 11:30:11 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int ac, char *av[])
                 for(int i = 0; av[j][i]; i++)
                 {
                     if(av[j][i] >= 'a' && av[j][i] <= 'z')
-                        av[j][i] -= 32 ;
+                        av[j][i] = std::toupper(av[j][i]);
                     std::cout << av[j][i];
                 } 
             }
--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,40 +6,8 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 00:39:54 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/09/28 23:59:35 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/09/29 11:30:29 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-
-
-#include <iostream>
-
-class FirstClass
-{
-    public:
-        FirstClass(); wwqwrqwere
-        ~FirstClass();b wrqwerqwer
-        void    print(std::string txt);
-};
-
-class SecondClass
-{
-    public:
-        Class();
-        ~SecondClass();
-        void    print(void);
-};
-
-int main(void)
-{  
-    
-    SecondClass obj1;
-    FirstClass obj2;
-
-
-    obj2.print("hello world");
-
-    return (0);
-}
